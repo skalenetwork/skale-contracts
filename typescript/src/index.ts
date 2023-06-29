e18936ce181e6c81dddbbd218371a631f7ff1550
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-06-29 19:02:41 +0300

    Add most important classes to default namespace

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,8 @@
 import { SkaleContracts } from "./skaleContracts";
 
+export { SkaleContracts } from "./skaleContracts";
+export { Network } from "./network";
+export { Project } from "./project";
+export { Instance } from "./instance";
+
 export const skaleContracts = new SkaleContracts();
