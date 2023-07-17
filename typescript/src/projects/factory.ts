7387ca3049a60ea8eda10a67091f6fdd455c4d58
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-07-17 17:51:48 +0300

    Fix issues in ProjectFactory

--- typescript/src/projects/factory.ts
+++ typescript/src/projects/factory.ts
@@ -1,24 +1,26 @@
 import { Network } from "../network";
 import { Project } from "../project";
+import {
+    ProjectNotFoundError
+} from "../domain/errors/project/projectNotFoundError";
 import { SkaleManagerProject } from "./skale-manager/skaleManagerProject";
 
 export const projects = {
-    skaleManager: {
-        name: "skale-manager",
-        path: "skale-manager"
+    "skaleManager": {
+        "name": "skale-manager",
+        "path": "skale-manager"
     }
-}
-
-class ProjectNotFoundError extends Error
-{}
+};
 
 class ProjectFactory {
-    create(network: Network, name: string): Project {
+    static create (network: Network, name: string): Project {
         if (name === projects.skaleManager.name) {
-            return new SkaleManagerProject(network, projects.skaleManager)
-        } else {
-            throw new ProjectNotFoundError(`Project with name ${name} is unknown`);
+            return new SkaleManagerProject(
+                network,
+                projects.skaleManager
+            );
         }
+        throw new ProjectNotFoundError(`Project with name ${name} is unknown`);
     }
 }
 
