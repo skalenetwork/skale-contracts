65de68a4c732a9478a648d17bcc78e8d2b916aff
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-07-17 17:45:51 +0300

    Fix issues in SkaleManagerInstance

--- typescript/src/projects/skale-manager/skaleManagerInstance.ts
+++ typescript/src/projects/skale-manager/skaleManagerInstance.ts
@@ -1,46 +1,105 @@
-import { ethers } from "ethers";
-import { Contract } from "ethers";
+import { Contract, ethers } from "ethers";
+import {
+    ContractAddress,
+    ContractName,
+    MainContractAddress
+} from "../../domain/types";
 import { Instance } from "../../instance";
-import { ContractAddress, ContractName, MainContractAddress } from "../../domain/types";
 import { Project } from "../../project";
 
 
-const skaleManagerAbi = [
-    { "inputs": [], "name": "contractManager", "outputs": [{ "internalType": "contract IContractManager", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
-    { "type": "function", "name": "version", "constant": true, "stateMutability": "view", "payable": false, "inputs": [], "outputs": [ { "type": "string", "name": "" } ] }
-]
-
 const contractManagerAbi = [
-    { "inputs": [{ "internalType": "string", "name": "name", "type": "string" }], "name": "getContract", "outputs": [{ "internalType": "address", "name": "contractAddress", "type": "address" }], "stateMutability": "view", "type": "function" }
-]
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                }
+            ],
+            "name": "getContract",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "contractAddress",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ],
+    skaleManagerAbi = [
+        {
+            "inputs": [],
+            "name": "contractManager",
+            "outputs": [
+                {
+                    "internalType": "contract IContractManager",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "version",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
 
 export class SkaleManagerInstance extends Instance {
     skaleManager: Contract;
 
     customNames = new Map<string, string>([
-        ["BountyV2", "Bounty"]
+        [
+            "BountyV2",
+            "Bounty"
+        ]
     ]);
 
     constructor (project: Project, address: MainContractAddress) {
-        super(project, address);
-        this.skaleManager = new ethers.Contract(this.address, skaleManagerAbi, this.provider);
+        super(
+            project,
+            address
+        );
+        this.skaleManager = new ethers.Contract(
+            this.address,
+            skaleManagerAbi,
+            this.provider
+        );
     }
 
-    async _getVersion() {
+    async _getVersion () {
         return await this.skaleManager.version() as string;
     }
 
-    async getContractAddress(name: ContractName): Promise<ContractAddress> {
-        const contractManagerAddress = await this.skaleManager.contractManager() as string;
-        const contractManager = new ethers.Contract(contractManagerAddress, contractManagerAbi, this.provider);
-        return await contractManager.getContract(this._actualName(name));
+    async getContractAddress (name: ContractName): Promise<ContractAddress> {
+        const contractManager = new ethers.Contract(
+            await this.skaleManager.contractManager() as string,
+            contractManagerAbi,
+            this.provider
+        );
+        return contractManager.getContract(this.actualName(name));
     }
 
-    _actualName(name: ContractName) {
+    // Private
+
+    private actualName (name: ContractName) {
         if (this.customNames.has(name)) {
             return this.customNames.get(name);
-        } else {
-            return name;
         }
+        return name;
     }
 }
