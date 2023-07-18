cd0f456bf62211b913c673f63fed7a35bf68d608
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-07-18 17:02:59 +0300

    Fix issues issues in Network

--- typescript/src/network.ts
+++ typescript/src/network.ts
@@ -1,37 +1,25 @@
-import { SkaleContracts } from "./skaleContracts";
 import { ProjectFactory } from "./projects/factory";
-import { Provider } from "@ethersproject/providers"
-
-export class NetworkNotFoundError extends Error
-{}
+import { Provider } from "@ethersproject/providers";
+import { SkaleContracts } from "./skaleContracts";
 
 export class Network {
-    private _skaleContracts: SkaleContracts;
-    private _provider: Provider;
+    private skaleContracts: SkaleContracts;
 
-    constructor(skaleContracts: SkaleContracts, provider: Provider) {
-        this._provider = provider;
-        this._skaleContracts = skaleContracts;
+    private networkProvider: Provider;
+
+    constructor (skaleContracts: SkaleContracts, provider: Provider) {
+        this.networkProvider = provider;
+        this.skaleContracts = skaleContracts;
     }
 
-    get provider() {
-        return this._provider;
+    get provider () {
+        return this.networkProvider;
     }
 
-    async getProject(name: string) {
-        return ProjectFactory.create(this, name);
-    }
-}
-
-export class ListedNetwork extends Network {
-    private _path;
-
-    constructor(skaleContracts: SkaleContracts, provider: Provider, path: string) {
-        super(skaleContracts, provider);
-        this._path = path;
-    }
-
-    get path() {
-        return this._path;
+    getProject (name: string) {
+        return ProjectFactory.create(
+            this,
+            name
+        );
     }
 }
