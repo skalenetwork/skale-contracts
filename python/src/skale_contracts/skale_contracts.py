416b02b93299affc0da4255e28e3b9c99c82ac84
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-07-19 17:03:37 +0300

    Fix issues in SkaleContracts

--- python/src/skale_contracts/skale_contracts.py
+++ python/src/skale_contracts/skale_contracts.py
@@ -1,19 +1,26 @@
+"""Contains SkaleContracts class"""
+
 from web3 import Web3
 from web3.providers import BaseProvider
 
-from metadata import Metadata
-from network import Network, ListedNetwork
+from .metadata import Metadata
+from .network import Network, ListedNetwork
 
 
 class SkaleContracts:
+    """Entry point of the SKALE Contracts library"""
     def __init__(self):
         self.metadata = Metadata()
 
     def get_network_by_provider(self, provider: BaseProvider) -> Network:
-        w3 = Web3(provider)
-        chain_id = w3.eth.chain_id
-        networkMetadata = self.metadata.get_network_by_chain_id(chain_id)
-        if networkMetadata is None:
-            return Network(self, provider)
-        else:
-            return ListedNetwork(self, provider, networkMetadata.path)
+        """Get network by provider"""
+        web3 = Web3(provider)
+        chain_id = web3.eth.chain_id
+        network_metadata = self.metadata.get_network_by_chain_id(chain_id)
+        if network_metadata:
+            return ListedNetwork(self, provider, network_metadata.path)
+        return Network(self, provider)
+
+    def get_network_by_http_endpoint(self, http_endpoint: str) -> Network:
+        """Get network by HTTP URI"""
+        return self.get_network_by_provider(Web3.HTTPProvider(http_endpoint))
