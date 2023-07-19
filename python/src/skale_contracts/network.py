4904a5c850f2a5bf7b97c347cfc5371d4ba5c701
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-07-19 17:22:34 +0300

    Fix issues in the network module

--- python/src/skale_contracts/network.py
+++ python/src/skale_contracts/network.py
@@ -1,17 +1,29 @@
+"""Module for work with networks"""
+
 from web3 import Web3
 from web3.providers import BaseProvider
 
-from projects.project_factory import project_factory
+from .projects.project_factory import project_factory
+
 
 class Network:
+    """Represents blockchain with deployed smart contracts projects"""
     def __init__(self, skale_contracts, provider: BaseProvider):
-        self.w3 = Web3(provider)
-        self._skaleContracts = skale_contracts
+        self.web3 = Web3(provider)
+        self._skale_contracts = skale_contracts
 
     def get_project(self, name: str):
+        """Get Project object by it's name"""
         return project_factory.create(self, name)
 
+    @property
+    def skale_contracts(self):
+        """Get SkaleContracts object associated with the network"""
+        return self._skale_contracts
+
+
 class ListedNetwork(Network):
+    """Network that is listed in the metadata"""
     def __init__(self, skale_contracts, provider: BaseProvider, path: str):
         super().__init__(skale_contracts, provider)
         self.path = path
