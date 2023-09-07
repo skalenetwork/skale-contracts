06339d7001e47301489b16088bc6e48dc7284a04
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-09-07 18:12:31 +0300

    Add Instance to ethers-v6

--- typescript/ethers-v6/src/skaleContracts.ts
+++ typescript/ethers-v6/src/skaleContracts.ts
@@ -1,9 +1,14 @@
 import { BaseContract, Provider, ethers } from "ethers";
 import {
+    Instance as BaseInstance
+} from "@skalenetwork/skale-contracts/lib/instance";
+import {
     SkaleContracts as BaseSkaleContracts
 } from "@skalenetwork/skale-contracts";
 import { Ethers6Adapter } from "./ethers6Adapter";
 
+
+export type Instance = BaseInstance<BaseContract>;
 
 export class SkaleContracts extends BaseSkaleContracts<BaseContract> {
     getNetworkByChainId (chainId: number) {
