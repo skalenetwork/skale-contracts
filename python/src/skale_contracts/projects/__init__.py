cbf068fd73089ce6d78dd72b323b064ac9234faf
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-04-11 14:49:17 +0000

    Add context-contract project

--- python/src/skale_contracts/projects/__init__.py
+++ python/src/skale_contracts/projects/__init__.py
@@ -1,8 +1,10 @@
 """Supported projects"""
+from .context_contract import ContextProject as Context
 from .ima import \
     MainnetImaProject as MainnetIma, \
     SchainImaProject as SchainIma
 from .skale_manager import SkaleManagerProject as SkaleManager
 from .skale_allocator import SkaleAllocatorProject as SkaleAllocator
 
-__all__ = ['MainnetIma', 'SchainIma', 'SkaleAllocator', 'SkaleManager']
+__all__ = ['Context', 'MainnetIma', 'SchainIma', 'SkaleAllocator',
+           'SkaleManager']
