e65cab6d9d0feb28934b699d63f5f7aee158a63d
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-08-09 13:11:40 +0000

    Added paymaster project

--- python/src/skale_contracts/projects/__init__.py
+++ python/src/skale_contracts/projects/__init__.py
@@ -1,10 +1,11 @@
 """Supported projects"""
 from .context import ContextProject as Context
+from .predeployed import PaymasterProject as Paymaster
 from .ima import \
     MainnetImaProject as MainnetIma, \
     SchainImaProject as SchainIma
 from .skale_manager import SkaleManagerProject as SkaleManager
 from .skale_allocator import SkaleAllocatorProject as SkaleAllocator
 
-__all__ = ['Context', 'MainnetIma', 'SchainIma', 'SkaleAllocator',
-           'SkaleManager']
+__all__ = ['Context', 'Paymaster', 'MainnetIma', 'SchainIma',
+           'SkaleAllocator', 'SkaleManager']
