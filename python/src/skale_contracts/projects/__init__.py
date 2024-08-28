94662cb19854f037400c5a609e364e7c3694546b
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-08-28 15:29:48 +0000

    Add ERC1820 support to python library

--- python/src/skale_contracts/projects/__init__.py
+++ python/src/skale_contracts/projects/__init__.py
@@ -1,6 +1,7 @@
 """Supported projects"""
 from .config_controller import ConfigControllerProject as ConfigController
 from .context import ContextProject as Context
+from .erc1820_predeployed import Erc1820PredeployedProject as Erc1820Predeployed
 from .etherbase import EtherbaseProject as Etherbase
 from .filestorage import FilestorageProject as FileStorage
 from .marionette import MarionetteProject as Marionette
@@ -11,6 +12,7 @@ from .ima import \
 from .skale_manager import SkaleManagerProject as SkaleManager
 from .skale_allocator import SkaleAllocatorProject as SkaleAllocator
 
-__all__ = ['ConfigController', 'Context', 'Etherbase', 'FileStorage',
+__all__ = ['ConfigController', 'Context', 'Etherbase',
+           'Erc1820Predeployed', 'FileStorage',
            'MainnetIma', 'Marionette', 'Paymaster', 'SchainIma',
            'SkaleAllocator', 'SkaleManager', ]
---
+++ python/src/skale_contracts/projects/erc1820_predeployed.py
@@ -0,0 +1,64 @@
+"""Module connects erc1820-predeployed project to the SKALE contracts library"""
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from eth_utils.address import to_canonical_address
+
+from skale_contracts.constants import PREDEPLOYED_ALIAS
+from skale_contracts.instance import Instance
+from skale_contracts.project import Project
+
+if TYPE_CHECKING:
+    from eth_typing import Address, ChecksumAddress
+
+
+class Erc1820PredeployedInstance(Instance):
+    """Represents instance of erc1820-predeployed"""
+
+    INITIAL_VERSION = '0.0.1-develop.1'
+
+    PREDEPLOYED: dict[str, Address] = {
+        name: to_canonical_address(address) for name, address in {
+            'Erc1820Predeployed':
+                '0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24'
+        }.items()}
+
+    def get_contract_address(
+            self,
+            name: str,
+            *args: str | Address | ChecksumAddress
+    ) -> Address:
+        if name in self.PREDEPLOYED:
+            return self.PREDEPLOYED[name]
+        raise RuntimeError(f"Can't get address of {name} contract")
+    
+    def _get_version(self) -> str:
+        try:
+            return super()._get_version()
+        except ValueError:
+            return self.INITIAL_VERSION
+
+
+class Erc1820PredeployedProject(Project):
+    """Represents erc1820-predeployed project"""
+
+    @staticmethod
+    def name() -> str:
+        return 'erc1820-predeployed'
+
+    def get_instance(self, alias_or_address: str) -> Instance:
+        if alias_or_address == PREDEPLOYED_ALIAS:
+            return self.create_instance(
+                Erc1820PredeployedInstance.PREDEPLOYED['Erc1820Predeployed']
+            )
+        return super().get_instance(alias_or_address)
+
+    @property
+    def github_repo(self) -> str:
+        return 'https://github.com/skalenetwork/erc1820-predeployed/'
+
+    def create_instance(self, address: Address) -> Instance:
+        return Erc1820PredeployedInstance(self, address)
+
+    def get_abi_filename(self, version: str) -> str:
+        return f'erc1820-predeployed-{version}-abi.json'
