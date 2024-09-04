254d10d1f1d8b0605b3c4d28a3b27faacf37e174
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-09-04 18:54:02 +0000

    Changed ERC1820 project naming

--- python/src/skale_contracts/projects/__init__.py
+++ python/src/skale_contracts/projects/__init__.py
@@ -1,7 +1,7 @@
 """Supported projects"""
 from .config_controller import ConfigControllerProject as ConfigController
 from .context import ContextProject as Context
-from .erc1820_predeployed import Erc1820PredeployedProject as Erc1820Predeployed
+from .erc1820 import Erc1820PredeployedProject as Erc1820Predeployed
 from .etherbase import EtherbaseProject as Etherbase
 from .filestorage import FilestorageProject as FileStorage
 from .marionette import MarionetteProject as Marionette
--- python/src/skale_contracts/projects/erc1820.py
+++ python/src/skale_contracts/projects/erc1820.py
@@ -1,4 +1,4 @@
-"""Module connects erc1820-predeployed project to the SKALE contracts library"""
+"""Module connects erc1820 project to the SKALE contracts library"""
 
 from __future__ import annotations
 from typing import TYPE_CHECKING
@@ -12,15 +12,15 @@ if TYPE_CHECKING:
     from eth_typing import Address, ChecksumAddress
 
 
-class Erc1820PredeployedInstance(Instance):
-    """Represents instance of erc1820-predeployed"""
+class Erc1820Instance(Instance):
+    """Represents instance of erc1820"""
 
     def _get_version(self) -> str:
       return '0.0.1-develop.1'
 
     PREDEPLOYED: dict[str, Address] = {
         name: to_canonical_address(address) for name, address in {
-            'Erc1820Predeployed':
+            'Erc1820':
                 '0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24'
         }.items()}
 
@@ -34,17 +34,17 @@ class Erc1820PredeployedInstance(Instance):
         raise RuntimeError(f"Can't get address of {name} contract")
 
 
-class Erc1820PredeployedProject(Project):
-    """Represents erc1820-predeployed project"""
+class Erc1820Project(Project):
+    """Represents erc1820 project"""
 
     @staticmethod
     def name() -> str:
-        return 'erc1820-predeployed'
+        return 'erc1820'
 
     def get_instance(self, alias_or_address: str) -> Instance:
         if alias_or_address == PREDEPLOYED_ALIAS:
             return self.create_instance(
-                Erc1820PredeployedInstance.PREDEPLOYED['Erc1820Predeployed']
+                Erc1820Instance.PREDEPLOYED['Erc1820']
             )
         return super().get_instance(alias_or_address)
 
@@ -53,7 +53,7 @@ class Erc1820PredeployedProject(Project):
         return 'https://github.com/skalenetwork/erc1820-predeployed/'
 
     def create_instance(self, address: Address) -> Instance:
-        return Erc1820PredeployedInstance(self, address)
+        return Erc1820Instance(self, address)
 
     def get_abi_filename(self, version: str) -> str:
         return f'erc1820-predeployed-{version}-abi.json'
