0e8acef7cbc9a63987d89f14fd0b0a6e80d2b93a
Oleksandr Salamatov <oleksandrsalamatov@skalelabs.com>
2024-09-05 12:42:36 +0000

    Changed name for Erc1820 project

--- python/src/skale_contracts/projects/__init__.py
+++ python/src/skale_contracts/projects/__init__.py
@@ -1,7 +1,7 @@
 """Supported projects"""
 from .config_controller import ConfigControllerProject as ConfigController
 from .context import ContextProject as Context
-from .erc1820 import Erc1820PredeployedProject as Erc1820Predeployed
+from .erc1820 import Erc1820Project as Erc1820
 from .etherbase import EtherbaseProject as Etherbase
 from .filestorage import FilestorageProject as FileStorage
 from .marionette import MarionetteProject as Marionette
@@ -12,7 +12,6 @@ from .ima import \
 from .skale_manager import SkaleManagerProject as SkaleManager
 from .skale_allocator import SkaleAllocatorProject as SkaleAllocator
 
-__all__ = ['ConfigController', 'Context', 'Etherbase',
-           'Erc1820Predeployed', 'FileStorage',
-           'MainnetIma', 'Marionette', 'Paymaster', 'SchainIma',
-           'SkaleAllocator', 'SkaleManager', ]
+__all__ = ['ConfigController', 'Context', 'Etherbase', 'Erc1820',
+           'FileStorage', 'MainnetIma', 'Marionette', 'Paymaster',
+           'SchainIma', 'SkaleAllocator', 'SkaleManager', ]
