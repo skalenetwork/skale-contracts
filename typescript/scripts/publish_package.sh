e5b253aa9436933fefcd8a03aee00a4afc87e65f
Vadim Yavorsky <yavrsky@gmail.com>
2025-01-15 13:36:22 +0000

    Add working directory reference to publish_package.sh for improved script reliability

--- typescript/scripts/publish_package.sh
+++ typescript/scripts/publish_package.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+WORKING_DIR=$(pwd)
 USAGE_MSG='Usage: BRANCH=[BRANCH] NODE_AUTH_TOKEN=[TOKEN] publish_package.sh'
 if [ -z "$BRANCH" ]
 then
@@ -45,6 +46,7 @@ mv package.json.new package.json
 
 # set reference to the base package
 if [ -z "$BASE_PACKAGE" ]; then
+    cd $WORKING_DIR
     yarn remove @skalenetwork/skale-contracts
     yarn add @skalenetwork/skale-contracts@$VERSION
 fi
