b9b67561a4b74c065e90ce13192a56d1bd0b7b74
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2026-03-11 15:21:58 +0000

    add re-exporting of domain types in packages

---
+++ typescript/base/src/domain/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./constants";
+export * from "./errors/instance/instanceNotFound";
+export * from "./errors/metadata/metadataIsNotDownloaded";
+export * from "./errors/network/networkNotFoundError";
+export * from "./errors/project/projectNotFoundError";
